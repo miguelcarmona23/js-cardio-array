eb3125bb37b74c90b535586927f20db47ccf9b3d
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-31 14:04:22 -0500

    Added .sort to find last names alphabetically

--- JS/script.js
+++ JS/script.js
@@ -46,4 +46,9 @@ const de = links
                 .filter(streetName => streetName.includes(`de`));
 */
 
-
+const alpha = people.sort((lastOne, nextOne) => {
+    const [aLast, aFirst] = lastOne.split(`,`);
+    const [bLast, bFirst] = nextOne.split(`,`);
+    return aLast > bLast ? 1 : -1;
+});
+console.log(alpha)
