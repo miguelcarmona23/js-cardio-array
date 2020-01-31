92a010a1e35df6ba7cea9de7ac4c3fe7d3fd294b
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-31 13:37:15 -0500

    Used .map and .filter to find streetname and link

--- JS/script.js
+++ JS/script.js
@@ -34,6 +34,11 @@ const oldest = inventors.sort(function(a,b){
 });
 console.table(oldest);
 
+const category = document.querySelector(`.mw-category`);
+const links = Array.from(category.querySelectorAll(`a`));
+const de = links
+                .map(links => links.textContent)
+                .filter(streetName => streetName.includes(`de`));
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
