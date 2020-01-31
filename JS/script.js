32d93dc0b2a7d52ba9ab3bd9facbbf6355a45d12
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-31 13:17:12 -0500

    Added a sort to find oldest and youngest from array

--- JS/script.js
+++ JS/script.js
@@ -20,6 +20,12 @@ console.table(fifteen);
 const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`)
 console.log(fullNames);
 
+
+
+const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
+
+console.table(ordered);
+
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
